39f13c5c8f54a36fffc314764269e9c67f640816
rajprins <rajprins@c45c0f7a-7b3e-0410-904d-7f450f53edee>
2009-08-07 14:24:40 +0000

    Minor bugfixing

--- trunk/pkill.pl
+++ trunk/pkill.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
-# 
+
 # A simple process viewer/killer for Linux, BSD and Solaris systems
 # Note: requires Perl/Tk
 #
-# Copyright (c) 2005, Roy Prins
-# All rights reserved.
+# Copyright (C) 2005, Roy Prins
+# All rights reserved
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 #
@@ -25,6 +25,7 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
+
 ################################################################################
 # uses and includes
 ################################################################################
@@ -39,7 +40,7 @@ use Tk::Font;
 ################################################################################
 my $OS            = $^O;
 my $PS_OPTIONS;
-my $REFRESHRATE   = 30000;
+my $REFRESHRATE   = 10000;
 my $USERNAME      = getpwuid $<;
 my $HEADER        = "USER | PID | COMMAND";
 my $TITLE         = "\u$OS Process Killer";
@@ -51,8 +52,10 @@ my $frame1;
 my $frame2;
 my $frame3;
 my $infoLabel;
+my $defaultOptions;
 my $filterField;
 my $filterButton;
+my $clearButton;
 my $quitButton;
 my $refreshButton;
 my $toggleButton;
@@ -103,19 +106,19 @@ sub checkOS {
       printf("Sorry, $OS is not supported. Exiting....\n");
       exit 1;
    }
+   $defaultOptions = $PS_OPTIONS;
 }
 
 
-### called when a field in the listbox is double-clicked.
-### PID is the first field in the listbox's active item.
-### The signal to send the process is set by the radiobuttons and stored in $signal.
+# Called when a field in the listbox is double-clicked.
+# PID is the first field in the listbox's active item.
+# The signal to send is set by the radiobuttons and stored in $signal.
 sub kill {
    (my $nothing, my $killpid) = split(/\ \|\ /, $process_list->get("active"));
    if ($killpid eq "1") {
-      $mainwindow->messageBox(-title=>"Oops", -message=>"Sorry, you cannot kill the init process!")
+      $mainwindow->messageBox(-title=>"Error", -message=>"Sorry, you cannot kill the init process!")
    }
    elsif ($killpid || $killpid ne "PID") {
-      #printf("Killing process $killpid using signal $signal.\n");
       system("kill -s $signal $killpid");
       refresh_list();
    }
@@ -125,7 +128,7 @@ sub kill {
 }
 
 
-### Switch views for showing only the current user's processes, or all processes
+# Switch views for showing only the current user's processes, or all processes
 sub toggleAll {
    if ($SHOWALL == 0) {
       # show all processes
@@ -141,21 +144,19 @@ sub toggleAll {
 }
 
 
-### Redraws the listbox with updated ps info
+# Redraws the listbox with updated ps info
 sub refresh_list{
    $process_list->delete(0, "end");
    getProcs();
 }
 
 
-### Get process info and store it in listbox
+# Get process info and store it in listbox
 sub getProcs {
    # get the actual process list
-   #printf("[DEBUG] Executing command: ps $PS_OPTIONS\n");
-
    @processes = `ps $PS_OPTIONS`;
 
-   # in "All users" mode, an extra header is generated. This fragment clears is out,
+   # in "All users" mode, an extra header is generated. This fragment clears it out,
    # but only if the ps command returns more than one line. (due to the filter option)
    my $size = @processes; 
    if ($SHOWALL == 1 && $size > 1) {
@@ -166,23 +167,22 @@ sub getProcs {
    $process_list->insert("0", $HEADER);
    $process_list->itemconfigure("0", -background=>"darkgrey");
 
-   $counter = 0;
-   
    # split output of the ps command and select the needed fields.
    # Linux and Mac OS X:
    # USER PID %CPU %MEM  VSZ RSS TTY STAT START TIME COMMAND
    # 0    1   2    3     4   5   6   7    8     9    10
+   #
    # Solaris with custom options (-o "user pid comm"):
    # UID PID COMM
    # 0   1   2
+   $counter = 0;
    foreach my $process (@processes) {
       (@fields) = split(/\s+/, $process);
-      $pid      = $fields[$pidfield];
       $owner    = $fields[$ownerfield];
+      $pid      = $fields[$pidfield];
       $command  = $fields[$commandfield];
       
-      # do not show processes that are not owner by current user if flag "-all"
-      # has not been set
+      # do not show processes that are not owned by current user if flag "-all" has not been set
       if ($SHOWALL == 0) {
             next unless($owner eq $USERNAME); 
       }
@@ -194,87 +194,94 @@ sub getProcs {
       if ($counter%2 != 0) {
          $process_list->itemconfigure($counter, -background=>"lightblue");
       }
-      # other color depends on personal taste or GUI theme
-      #else {
-      #   $process_list->itemconfigure($counter, -background=>"white");
-      #}
+      else {
+         $process_list->itemconfigure($counter, -background=>"lightyellow");
+      }
    }
 }
 
 
-### process search string, narrowing the process list
+sub clearFilter {
+   $filterField->delete("0", "end");
+   $PS_OPTIONS = $defaultOptions;
+   refresh_list;
+}
+
+
+# process search string, narrowing the process list
 sub doFilter {
    my ($widget) = @_;
    my $filter = $widget->get();
 
+   $PS_OPTIONS = $defaultOptions;
    if (! $filter eq "") {
       # backup of original options
-      my $ps_options = $PS_OPTIONS;
+      $defaultOptions = $PS_OPTIONS;
       # setting the new options
       $PS_OPTIONS = $PS_OPTIONS." | grep -i ".$filter." | grep -v grep";
       refresh_list;
       # restore original search options
-      $PS_OPTIONS = $ps_options;
+      #$PS_OPTIONS = $ps_options;
    }
    else {
-      refresh_list;
+      clearFilter;
    }
 }
+
 
 
 ################################################################################
 # main logic
 ################################################################################
 
-### First, check if the "-all" argument was given
+# First, check if the "-all" argument was given
 if ((@ARGV != 0) && ($ARGV[0] eq "-a")) {
    $SHOWALL = 1;
 }
 
-
-### Next, check OS version, since not OSes are supported (eg. Windows)
+# Next, check OS version, since not OSes are supported (eg. Windows)
 checkOS;
 
-
-### main window
+# main window
 $mainwindow = MainWindow->new(); 
-# change value of borderwidth into 2 for the "old skool" motif look
-$mainwindow->optionAdd("*BorderWidth"=>1);
+# change value of borderwidth into '2' for the "old skool" motif look
+# or into '1' for a more modern look
+$mainwindow->optionAdd("*BorderWidth"=>2);
 $mainwindow->title($TITLE." (refresh rate: ".($REFRESHRATE/1000)." seconds)");
 
-
-### frame for holding help text
+# frame for holding help text
 my $instructions = "Instructions: Select a signal and then double-click on the process you wish to terminate.";
 $frame0 = $mainwindow->Frame(-background=>"darkcyan", -relief=>"raised")->pack(-side=>"top", -fill=>"x");
 $frame0->Label(-background=>"darkcyan", -foreground=>"white", -text=>$instructions)->pack();
 
-
-### frame to hold radiobuttons for choosing a signal.
+# frame to hold radiobuttons for choosing a signal.
 $frame1 = $mainwindow->Frame()->pack();
 $frame1->Label(-text=>"Signal:")->pack(-side=>"left");
+
 # default selection will be "Terminate" 
 $signal = "TERM";
 $frame1->Radiobutton(-variable=>\$signal, -text=> "Terminate", -value=>"TERM")->pack(-side=>"left");
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Stop",      -value=>"STOP")->pack(-side=>"left");
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Continue",  -value=>"CONT")->pack(-side=>"left");
+$frame1->Radiobutton(-variable=>\$signal, -text=> "Stop", -value=>"STOP")->pack(-side=>"left");
+$frame1->Radiobutton(-variable=>\$signal, -text=> "Continue", -value=>"CONT")->pack(-side=>"left");
 $frame1->Radiobutton(-variable=>\$signal, -text=> "Interrupt", -value=>"INT")->pack(-side=>"left");
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Hangup",    -value=>"HUP")->pack(-side=>"left");
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Kill",      -value=>"KILL")->pack(-side=>"left");
+$frame1->Radiobutton(-variable=>\$signal, -text=> "Hangup", -value=>"HUP")->pack(-side=>"left");
+$frame1->Radiobutton(-variable=>\$signal, -text=> "Kill", -value=>"KILL")->pack(-side=>"left");
 
 # frame with scrollbar for process list
 $frame2 = $mainwindow->Frame()->pack(-fill=>"both", -expand=>"y");
 
 #bottom frame for refresh button
-$frame3        = $mainwindow->Frame()->pack(-fill=>"both", -expand=>"n");
-$filterField   = $frame3->Entry()->pack(-side=>'left');
-$filterButton  = $frame3->Button(-text=>"Filter", -command=>sub{doFilter($filterField)})->pack(-side=>'left');
-$quitButton    = $frame3->Button(-text=>"Quit", -command=>\&exit)->pack(-side=>'right');
+$frame3  = $mainwindow->Frame()->pack(-fill=>"both", -expand=>"n");
+$filterField = $frame3->Entry()->pack(-side=>'left');
+$filterButton = $frame3->Button(-text=>"Filter", -command=>sub{doFilter($filterField)})->pack(-side=>'left');
+$clearButton = $frame3->Button(-text=>"Clear", -command=>\&clearFilter)->pack(-side=>'left');
+$quitButton = $frame3->Button(-text=>"Quit", -command=>\&exit)->pack(-side=>'right');
 $refreshButton = $frame3->Button(-text=>"Refresh", -command=>\&refresh_list)->pack(-side=>'right');
-$toggleButton  = $frame3->Button(-text=>"All Users", -command=>\&toggleAll)->pack(-side=>'right');
+$toggleButton = $frame3->Button(-text=>"All Users", -command=>\&toggleAll)->pack(-side=>'right');
 
 # create listbox and add to frame2
 # note: this size actually determines the application size
-$process_list = $frame2->Listbox(-height=>13)->pack(-side=>"left", -fill=>"both", -expand=>"y");
+$process_list = $frame2->Listbox(-height=>20)->pack(-side=>"left", -fill=>"both", -expand=>"y");
 
 # get ps info and display selected fields in listbox
 getProcs();
