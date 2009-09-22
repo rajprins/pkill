e3cb40dd6e9dfac3214144fe923bc7eb3688019d
rajprins@gmail.com <rajprins@gmail.com@c45c0f7a-7b3e-0410-904d-7f450f53edee>
2009-09-22 18:28:13 +0000

    Improved stability, error handling and minor interface tweaks

--- trunk/pkill.pl
+++ trunk/pkill.pl
@@ -4,6 +4,30 @@
 # Note: requires Perl/Tk
 #
 # Copyright (c) 2005, Roy Prins
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY <copyright holder> ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 ################################################################################
@@ -17,16 +41,19 @@ use Tk::PNG;
 
 
 ################################################################################
-# Variables
+# Constants & Variables
 ################################################################################
-my $OS            = $^O;
-my $PS_OPTIONS;
-my $REFRESHRATE   = 10000;
-my $USERNAME      = getpwuid $<;
-my $HEADER        = "USER | PID | COMMAND";
-my $TITLE         = "\u$OS Process Killer";
-my $SHOWALL       = 0;
+my $OS          = $^O;
+my $REFRESHRATE = 30; #seconds
+my $USERNAME    = getpwuid $<;
+my $HEADER      = "USER | PID | COMMAND";
+my $TITLE       = "\u$OS Process Killer";
+my $ICON        = "camel.png";
 
+my $ps_args;
+my $ps_args_bak;
+my $showAll;
+my $filtered;
 my $mainwindow;
 my $icon;
 my $instructions;
@@ -38,12 +65,10 @@ my $frame4;
 my $infoLabel;
 my $filterField;
 my $filterButton;
-my $filtered = 0;
 my $clearButton;
 my $quitButton;
 my $refreshButton;
 my $toggleButton;
-my $ps_options;
 my @processes;
 my $process;
 my $process_list;
@@ -64,26 +89,41 @@ my $counter;
 # Subroutines
 ################################################################################
 
+# Perform some basic initialization
+sub init {
+   # by default, the process list is not filtered
+   $filtered = 0;
+   
+   # check if the "-all" argument was given
+   if ((@ARGV != 0) && ($ARGV[0] eq "-a")) {
+      $showAll = 1;
+   }
+   else {
+      $showAll = 0;   
+   }
+}
+
+
 # check OS version in order to determine some OS specific settings
 # For example, Windows is not supported....
 sub checkOS {
    if ($OS eq "linux")  {
       # Linux
-      $PS_OPTIONS = "aux";  
+      $ps_args = "aux";  
       $pidfield = 1;
       $ownerfield = 0;
       $commandfield = 10;
    }
    elsif ($OS eq "darwin") { 
       # Mac OS X/Darwin/OpenDarwin
-      $PS_OPTIONS = "-auxc"; 
+      $ps_args = "-auxc"; 
       $pidfield = 1;
       $ownerfield = 0;
       $commandfield = 10;
    }
    elsif ($OS eq "solaris") { 
       # Sun Solaris/OpenSolaris (but not Nexenta!)
-      $PS_OPTIONS = '-e -o "user pid comm"'; 
+      $ps_args = '-e -o "user pid comm"'; 
       $pidfield = 1;
       $ownerfield = 0;
       $commandfield = 2;
@@ -96,13 +136,13 @@ sub checkOS {
 
 # called when a field in the listbox is double-clicked.
 # PID is the first field in the listbox's active item.
-# The signal to send the process is set by the radiobuttons and stored in $signal.
+# The signal to send the process is set by the radiobuttons and stored in $signal
 sub kill {
    $process = $process_list->get("active");
    (my $nothing, my $killpid) = split(/\ \|\ /, $process_list->get("active"));
    if ($killpid eq "1") {
-      my $msg = "You cannot kill the init process!\n\nThis system depends on the init process. Killing it would result in a system failure.";
-      $mainwindow->messageBox(-font=>"Ansi 9", -type=>"OK", -title=>"Error", -message=>$msg)
+      my $msg = "Cannot kill the init process!\n\nThis system depends on the init process. Killing it would result in a system failure.";
+      $mainwindow->messageBox(-background=>"red", -font=>"Ansi 9", -type=>"OK", -title=>"Error", -message=>$msg)
    }
    elsif ($nothing ne $USERNAME) {
       my $msg = "System error:\n\nYou cannot kill processes that are not yours.";
@@ -121,15 +161,16 @@ sub kill {
 }
 
 # Switch views for showing only the current user's processes, or all processes
+# Note that it's not allowed to kill processes not belonging to the current user
 sub toggleAll {
-   if ($SHOWALL == 0) {
+   if ($showAll == 0) {
       # show all processes
-      $SHOWALL = 1;
+      $showAll = 1;
       $toggleButton->configure(-text => "Current User");
    }
    else {
       # show procs of current user only
-      $SHOWALL = 0;
+      $showAll = 0;
       $toggleButton->configure(-text => "All Users");
    }
    refresh_list();
@@ -144,12 +185,12 @@ sub refresh_list{
 # Get process info and store it in listbox
 sub getProcs {
    # get the actual process list
-   @processes = `ps $PS_OPTIONS`;
+   @processes = `ps $ps_args`;
 
    # in "All users" mode, an extra header is generated. This fragment clears is out,
    # but only if the ps command returns more than one line. (due to the filter option)
    my $size = @processes; 
-   if ($SHOWALL == 1 && $size > 1) {
+   if ($showAll == 1 && $size > 1) {
          shift @processes;
    }   
 
@@ -174,17 +215,19 @@ sub getProcs {
       $command  = $fields[$commandfield];
       
       # do not show processes that are not owned by current user if flag "-all" has not been set
-      if ($SHOWALL == 0) {
+      if ($showAll == 0) {
             next unless($owner eq $USERNAME); 
       }
       
+      # add process info to list
       $process_list->insert("end", "$owner | $pid | $command");
       
-      # change the background color of every other line into blue   
       $counter++;
+      # change the background color of every other line into blue...         
       if ($counter%2 != 0) {
          $process_list->itemconfigure($counter, -background=>"lightblue");
       }
+      # ...or yellow
       else {
          $process_list->itemconfigure($counter, -background=>"lightyellow");
       }
@@ -200,127 +243,129 @@ sub doFilter {
       # Check if the process list already has been filtered.
       # if so, first restore the orignal list in order to prevent filtering on filtering
       if ($filtered == 1) {
-         $PS_OPTIONS = $ps_options;
+         # safe to assume that $ps_args_bak has been set
+         $ps_args = $ps_args_bak;
       }
       
       # change the appearance of the button
       $filterButton->configure(-relief => "sunken"); 
       
       # backup of original options
-      $ps_options = $PS_OPTIONS;
+      $ps_args_bak = $ps_args;
       
       # setting the new options
-      $PS_OPTIONS = $PS_OPTIONS." | grep -i ".$filter." | grep -v grep";
+      $ps_args = $ps_args." | grep -i ".$filter." | grep -v grep";
       $filtered = 1;
       
       refresh_list;
    }
    else {
-      refresh_list;
+      #print "No filter string defined.\n";
    }
 }
 
 # Undo the filtering on the ps command
 sub clearFilter {
-   #restore the original look of the filter button
-   $filterButton->configure(-relief => "raised"); 
+   if ($filtered == 1) {
+      #restore the original look of the filter button
+      $filterButton->configure(-relief => "raised"); 
    
-   $filterField->delete(0, 'end');
-   # restore original search options
-   $PS_OPTIONS = $ps_options;
-   $filtered = 0;
+      $filterField->delete(0, 'end');
+      # restore original search options
+      $ps_args = $ps_args_bak;
+      $filtered = 0;
    
-   refresh_list;
+      refresh_list;
+   }
+   else {
+      #print "No filter defined. No sense in clearing...\n";
+   }
 
 }
 
-# Check if a Perl icon is available. If so, include it in the window
-sub checkResources {
-   if (-e "perl.png") {
-      $icon = $mainwindow->Photo(-file=>"perl.png", -format=>"PNG");
+sub setMainWindow {
+   $mainwindow = MainWindow->new();  
+   #$mainwindow->configure(-width=>"10", -height=>"10");
+   $mainwindow->title($TITLE." (refresh rate: ".$REFRESHRATE." seconds)");
+   # Window is not resizable
+   $mainwindow->resizable(0,0);
+   # change value of borderwidth into '2' for the "old skool" motif look or into '1' for a more modern look
+   $mainwindow->optionAdd("*BorderWidth"=>2);
+   
+   # Check if a Perl icon is available. If so, include it in the window
+   if (-e $ICON) {
+      $icon = $mainwindow->Photo(-file=>$ICON, -format=>"PNG");
       $mainwindow->Icon(-image=>$icon);
    }
-   elsif (-e "/usr/share/icons/perl.png") {
-      $icon = $mainwindow->Photo(-file=>"/usr/share/icons/perl.png", -format=>"PNG");
+   elsif (-e "/usr/share/icons/$ICON") {
+      $icon = $mainwindow->Photo(-file=>"/usr/share/icons/$ICON", -format=>"PNG");
       $mainwindow->Icon(-image=>$icon);
    }
    
 }
+
+sub setLayout {
+   # Frame0: frame for holding help text
+   $instructions = "Instructions: Select a signal and then double-click on the process you wish to terminate.";
+   $frame0 = $mainwindow->Frame(-background=>"darkcyan", -relief=>"raised")->pack(-side=>"top", -fill=>"both");
+   $frame0->Label(-background=>"darkcyan", -foreground=>"white", -text=>$instructions)->pack();
+   
+   # Frame1: frame to hold radiobuttons for choosing a signal. Default selected is "Terminate"
+   $signal = "TERM";
+   $frame1 = $mainwindow->Frame()->pack();
+   $frame1->Label(-text=>"Signal:")->pack(-side=>"left");
+   $frame1->Radiobutton(-variable=>\$signal, -text=> "Terminate", -value=>"TERM")->pack(-side=>"left");
+   $frame1->Radiobutton(-variable=>\$signal, -text=> "Stop", -value=>"STOP")->pack(-side=>"left");
+   $frame1->Radiobutton(-variable=>\$signal, -text=> "Continue", -value=>"CONT")->pack(-side=>"left");
+   $frame1->Radiobutton(-variable=>\$signal, -text=> "Interrupt", -value=>"INT")->pack(-side=>"left");
+   $frame1->Radiobutton(-variable=>\$signal, -text=> "Hangup", -value=>"HUP")->pack(-side=>"left");
+   $frame1->Radiobutton(-variable=>\$signal, -text=> "Kill", -value=>"KILL")->pack(-side=>"left");
+   
+   # Frame2: frame for process list
+   # note: size of listbox actually determines the application size
+   $frame2 = $mainwindow->Frame()->pack(-fill=>"both", -expand=>"y");
+   $process_list = $frame2->Listbox(-height=>20)->pack(-side=>"left", -fill=>"both", -expand=>"y");
+   $vertScrollbar = $frame2->Scrollbar(-orient=>"vertical", -width=>10, -command=>["yview", $process_list], )->pack(-side=>"right", -fill=>"y");
+   # bind a double-click on the listbox to the kill() subroutine.
+   $process_list->bind("<Double-1>", \&kill);
+   
+   # Frame3: extra frame for horizontal scrollbar
+   $frame3 = $mainwindow->Frame()->pack(-side=>"top", -fill=>"both", -expand=>"y");
+   $horScrollbar = $frame3->Scrollbar(-orient=>"horizontal", -width=>10, -command=>["xview", $process_list], )->pack(-side=>"bottom", -fill=>"both");
+   
+   # bind scrollbars to process list
+   $process_list->configure(-xscrollcommand=>["set", $horScrollbar], -yscrollcommand=>["set", $vertScrollbar]);
+   
+   # Frame4: bottom frame for function buttons
+   $frame4 = $mainwindow->Frame()->pack(-fill=>"both", -expand=>"n");
+   #$filterField = $frame4->Entry()->pack(-side=>'left');
+   $filterField = $frame4->Entry(-borderwidth=>"1", -highlightbackground=>"grey", -highlightcolor=>"grey")->pack(-side=>'left');
+   
+   $filterButton = $frame4->Button(-text=>"Filter", -command=>sub{doFilter($filterField)})->pack(-side=>'left');
+   $clearButton = $frame4->Button(-text=>"Clear", -command=>sub{clearFilter()})->pack(-side=>"left");
+   $quitButton = $frame4->Button(-text=>"Quit", -command=>\&exit)->pack(-side=>'right');
+   $refreshButton = $frame4->Button(-text=>"Refresh", -command=>\&refresh_list)->pack(-side=>'right');
+   $toggleButton = $frame4->Button(-text=>"All Users", -command=>\&toggleAll)->pack(-side=>'right');
+}
+
+
+sub setContent {
+   # get process info and display selected fields in listbox
+   getProcs();
+
+   # refresh the processlist list every X seconds, where X is $REFRESHRATE * 1000
+   $process_list->repeat(($REFRESHRATE*1000),\&refresh_list);   
+}
+
 
 ################################################################################
 ### main logic
 ################################################################################
-
-# First, check if the "-all" argument was given
-if ((@ARGV != 0) && ($ARGV[0] eq "-a")) {
-   $SHOWALL = 1;
-}
-
-# Next, check OS version, since not OSes are supported (eg. Windows)
+init;
 checkOS;
-
-# main window
-$mainwindow = MainWindow->new(); 
-$mainwindow->title($TITLE." (refresh rate: ".($REFRESHRATE/1000)." seconds)");
-$mainwindow->resizable(0,0);
-
-# check if the nice Perl icon is available
-checkResources();
-
-# change value of borderwidth into '2' for the "old skool" motif look or into '1' for a more modern look
-$mainwindow->optionAdd("*BorderWidth"=>2);
-
-# frame for holding help text
-$instructions = "Instructions: Select a signal and then double-click on the process you wish to terminate.";
-$frame0 = $mainwindow->Frame(-background=>"darkcyan", -relief=>"raised")->pack(-side=>"top", -fill=>"x");
-$frame0->Label(-background=>"darkcyan", -foreground=>"white", -text=>$instructions)->pack();
-
-# frame to hold radiobuttons for choosing a signal.
-$frame1 = $mainwindow->Frame()->pack();
-$frame1->Label(-text=>"Signal:")->pack(-side=>"left");
-
-# default selection will be "Terminate" 
-$signal = "TERM";
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Terminate", -value=>"TERM")->pack(-side=>"left");
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Stop",      -value=>"STOP")->pack(-side=>"left");
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Continue",  -value=>"CONT")->pack(-side=>"left");
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Interrupt", -value=>"INT")->pack(-side=>"left");
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Hangup",    -value=>"HUP")->pack(-side=>"left");
-$frame1->Radiobutton(-variable=>\$signal, -text=> "Kill",      -value=>"KILL")->pack(-side=>"left");
-
-# frame for process list
-$frame2 = $mainwindow->Frame()->pack(-fill=>"both", -expand=>"y");
-
-# extra frame for horizontal scrollbar
-$frame4 = $mainwindow->Frame()->pack(-side=>"top", -fill=>"both", -expand=>"y");
-
-#bottom frame for refresh button
-$frame3        = $mainwindow->Frame()->pack(-fill=>"both", -expand=>"n");
-$filterField   = $frame3->Entry()->pack(-side=>'left');
-$filterButton  = $frame3->Button(-text=>"Filter", -command=>sub{doFilter($filterField)})->pack(-side=>'left');
-$clearButton   = $frame3->Button(-text=>"Clear", -command=>sub{clearFilter()})->pack(-side=>"left");
-$quitButton    = $frame3->Button(-text=>"Quit", -command=>\&exit)->pack(-side=>'right');
-$refreshButton = $frame3->Button(-text=>"Refresh", -command=>\&refresh_list)->pack(-side=>'right');
-$toggleButton  = $frame3->Button(-text=>"All Users", -command=>\&toggleAll)->pack(-side=>'right');
-
-# create listbox and add to frame2
-# note: this size actually determines the application size
-$process_list = $frame2->Listbox(-height=>20)->pack(-side=>"left", -fill=>"both", -expand=>"y");
-
-# get ps info and display selected fields in listbox
-getProcs();
-
-# bind a double-click on the listbox to the kill() subroutine.
-$process_list->bind("<Double-1>", \&kill);
-
-# create a vertical scrollbar for listbox (always present)
-$horScrollbar = $frame4->Scrollbar(-orient=>"horizontal", -width=>10, -command=>["xview", $process_list], )->pack(-side=>"bottom", -fill=>"both");
-$vertScrollbar = $frame2->Scrollbar(-orient=>"vertical", -width=>10, -command=>["yview", $process_list], )->pack(-side=>"right", -fill=>"y");
-$process_list->configure(-xscrollcommand=>["set", $horScrollbar], -yscrollcommand=>["set", $vertScrollbar]);
-
-# refresh the PS list every X seconds, where X is $REFRESHRATE
-$process_list->repeat($REFRESHRATE,\&refresh_list);
+setMainWindow;
+setLayout;
+setContent;
 
 MainLoop();
-
 #EOF
